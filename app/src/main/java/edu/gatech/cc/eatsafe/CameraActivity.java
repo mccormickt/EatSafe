2ff3a32f454aeaa26f7a3124e9c8280b63473751
Thomas McCormick <tmccormick30@gatech.edu>
2018-09-18 23:10:58 -0400

    Optomize imports

--- app/src/main/java/edu/gatech/cc/eatsafe/CameraActivity.java
+++ app/src/main/java/edu/gatech/cc/eatsafe/CameraActivity.java
@@ -1,12 +1,10 @@
 package edu.gatech.cc.eatsafe;
 
-import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
 import butterknife.BindView;
